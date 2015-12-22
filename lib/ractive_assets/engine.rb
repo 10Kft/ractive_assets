98ddded93061478b945fb798d1daebeea6bcfe2e
Andy Camp <andoq@users.noreply.github.com>
2015-12-22 10:57:05 -0800

    Fix for latest version of rails and sprockets

--- lib/ractive_assets/engine.rb
+++ lib/ractive_assets/engine.rb
@@ -1,11 +1,16 @@
+# Andy updated this to work with latest versions of rails and sprockets.  This was originally copied after handlebars-assets, 
+# but is long out of date, and I used that as a template for how to update:
+# https://github.com/leshill/handlebars_assets/blob/master/lib/handlebars_assets/engine.rb
+
 module RactiveAssets
   class Engine < ::Rails::Engine
-    initializer "sprockets.ractive", :after => "sprockets.environment", :group => :all do |app|
-      next unless app.assets
-      app.assets.register_engine '.rac',         TiltRactive
-      app.assets.register_engine '.ractive',     TiltRactive
-      app.assets.register_engine('.ractivehaml', TiltRactive) if RactiveAssets::Config.haml_available?
-      app.assets.register_engine('.ractiveslim', TiltRactive) if RactiveAssets::Config.slim_available?
+    initializer "sprockets.ractive", :group => :all do |app|
+      config.assets.configure do |sprockets_env|
+        sprockets_env.register_engine '.rac',         TiltRactive
+        sprockets_env.register_engine '.ractive',     TiltRactive
+        sprockets_env.register_engine('.ractivehaml', TiltRactive) if RactiveAssets::Config.haml_available?
+        sprockets_env.register_engine('.ractiveslim', TiltRactive) if RactiveAssets::Config.slim_available?
+      end
     end
   end
 end
